37553f5329f65c8e8ee44b15b184287720e6d57f
Jhoana Aquino <jhoana.aquino10@gmail.com>
2025-05-30 14:40:59 -0500

    actiu

--- src/app/components/estadisticas/estadisticas.component.ts
+++ src/app/components/estadisticas/estadisticas.component.ts
@@ -1,3 +1,18 @@
+/*
+1: en juegos
+2:componentes maneja todo los detalles pero en services estan las categorias
+3:
+4.2
+1: porque ya tenemos standalone
+2: porque no facilita la comunicación  entre componentes. 
+
+
+
+*/
+
+
+
+
 import { Component } from '@angular/core';
 
 @Component({
